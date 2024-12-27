4f2085a8c9de9410d5437bddf08753e082f68b46
caocaocc <129647241+caocaocc@users.noreply.github.com>
2024-12-27 20:06:45 +0800

    Update main.go

--- main.go
+++ main.go
@@ -60,10 +60,10 @@ func get(downloadURL *string) ([]byte, error) {
 
 func download(release *github.RepositoryRelease) ([]byte, error) {
 	geositeAsset := common.Find(release.Assets, func(it *github.ReleaseAsset) bool {
-		return *it.Name == "dlc.dat"
+		return *it.Name == "geosite.dat"
 	})
 	geositeChecksumAsset := common.Find(release.Assets, func(it *github.ReleaseAsset) bool {
-		return *it.Name == "dlc.dat.sha256sum"
+		return *it.Name == "geosite.dat.sha256sum"
 	})
 	if geositeAsset == nil {
 		return nil, E.New("geosite asset not found in upstream release ", release.Name)
@@ -422,8 +422,8 @@ func release(source string, destination string, output string, cnOutput string, 
 
 func main() {
 	err := release(
-		"v2fly/domain-list-community",
-		"sagernet/sing-geosite",
+		"caocaocc/domain-list-custom",
+		"caocaocc/geosite",
 		"geosite.db",
 		"geosite-cn.db",
 		"rule-set",
